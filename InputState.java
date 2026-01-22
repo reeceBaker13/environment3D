f1b98a6165f5742cc1a2dff715b309b35d405245
IdioticIdiom <106718797+IdioticIdiom@users.noreply.github.com>
2026-01-22 05:06:30 +0000

    Redistributed responsibilities appropriately between the MovementManager and the InputManager

--- InputState.java
+++ InputState.java
@@ -1,2 +1,8 @@
-public class InputState {
+public final class InputState {
+    public float moveX;
+    public float moveZ;
+    public float lookX;
+    public float lookY;
+    public boolean jumping;
+    public boolean sprinting;
 }
